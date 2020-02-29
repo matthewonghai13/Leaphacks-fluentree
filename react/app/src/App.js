b3516f3d527833a576e34fb67348198557b24692
Matthew Onghai <matthewonghai@utexas.edu>
2020-02-29 11:21:48 -0600

    connected bootstrap to frontend

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// bootstrap dependencies
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
