c27358581da5a3a6cfa800e4eb84aeb978ed6121
Matthew Onghai <matthewonghai@utexas.edu>
2020-02-29 11:08:45 -0600

    restructured folders

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -8,8 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test
         </p>
+      
+
         <a
           className="App-link"
           href="https://reactjs.org"
