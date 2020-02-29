00d7602b834a64cfb8cee17bf31a5d9af8f4148f
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-02-29 17:29:27 -0600

    Border for languages

--- react/app/src/components/LangMenu.js
+++ react/app/src/components/LangMenu.js
@@ -18,9 +18,9 @@ import {Link} from "react-router-dom";
 
 // login page for webapp
 
-var spanish = false;
-var french = false;
-var japanese = false;
+// var spanish = false;
+// var french = false;
+// var japanese = false;
 
 var work = false;
 var travel = false;
@@ -28,81 +28,127 @@ var culture = false;
 
 var spanishClass = "card";
 
-function LangMenu() {
-  return (
-    <body>
+class LangMenu extends React.Component {
+  constructor() {
+    super();
+      this.state={
+        spanish: false,
+        french: false,
+        japanese: false,
+        work: false,
+        travel: false,
+        culture: false
+    };
+    this.selectLanguage = this.selectLanguage.bind(this);
+  }
+  // sets global language variables for submission
+  selectLanguage(langToSelect) { 
+    switch(langToSelect) {
+      case "spanish":
+        this.setState({spanish: !this.state.spanish})
+        this.setState({french: false})
+        this.setState({japanese: false})
+        break;
+      case "french":
+        this.setState({french: !this.state.french})
+        this.setState({spanish: false})
+        this.setState({japanese: false})
+        break;
+      case "japanese":
+        this.setState({japanese: !this.state.japanese})
+        this.setState({french: false})
+        this.setState({spanish: false})
 
-      {/* pick language */}
-      <div class = "mainDiv">
-        <h1>Pick a Language</h1>
-      </div>
+        break;
+      default:
+        break;
+    }
+  
+    
+  
+    // alert("" + this.state.spanish + this.state.french + this.state.japanese);
+  }
 
-      <div class="card-deck">
-        <div class={"card "} onClick={()=>{ selectLanguage('spanish'); }}>
-          <img src = {spain} class="card-img-top" alt="spain"></img>
-          <div class="card-body">
-            <h5 class="card-title">Spanish</h5>
-            <p class="card-text">¡Hola! Me alegra tenerte aquí.<br></br>
-            Haga clic para continuar su aprendizaje.</p>
-          </div>
+  render() {
+    
+    let spanishClass = this.state.spanish ? "selected card" : "unselected card";
+    let frenchClass = this.state.french ? "selected card" : "unselected card";
+    let japaneseClass = this.state.japanese ? "selected card" : "unselected card";
+    return (
+      <body>
+        
+        {/* pick language */}
+        <div class = "mainDiv">
+          <h1>Pick a Language</h1>
         </div>
-        <div class="card" onClick={()=>{ selectLanguage('french'); }}>
-          <img src = {france} class="card-img-top" alt="france"></img>
-          <div class="card-body">
-            <h5 class="card-title">French</h5>
-            <p class="card-text">salut! Heureux de vous avoir ici.<br></br>
-            Cliquez pour continuer votre apprentissage</p>
-          </div>
-        </div>
-        <div class="card" onClick={()=>{ selectLanguage('japanese'); }}>
-          <img src = {japan} class="card-img-top" alt="japan"></img>
-          <div class="card-body">
-            <h5 class="card-title">Japanese</h5>
-            <p class="card-text">こんにちは！ ここにいてよかった。<br></br>
-            クリックして学習を続けます</p>
-          </div>
-        </div>
-      </div>
 
-      {/* end pick language */}
+        <div class="card-deck">
+          <div id="spanish" class={spanishClass} onClick={()=>{ this.selectLanguage('spanish'); }}>
+            <img src = {spain} class="card-img-top" alt="spain"></img>
+            <div class="card-body">
+              <h5 class="card-title">Spanish</h5>
+              <p class="card-text">¡Hola! Me alegra tenerte aquí.<br></br>
+              Haga clic para continuar su aprendizaje.</p>
+            </div>
+          </div>
+          <div id="french" class={frenchClass} onClick={()=>{ this.selectLanguage('french'); }}>
+            <img src = {france} class="card-img-top" alt="france"></img>
+            <div class="card-body">
+              <h5 class="card-title">French</h5>
+              <p class="card-text">salut! Heureux de vous avoir ici.<br></br>
+              Cliquez pour continuer votre apprentissage</p>
+            </div>
+          </div>
+          <div id="japanese" class={japaneseClass} onClick={()=>{ this.selectLanguage('japanese'); }}>
+            <img src = {japan} class="card-img-top" alt="japan"></img>
+            <div class="card-body">
+              <h5 class="card-title">Japanese</h5>
+              <p class="card-text">こんにちは！ ここにいてよかった。<br></br>
+              クリックして学習を続けます</p>
+            </div>
+          </div>
+        </div>
 
-      {/* pick reason */}
-      <div class = "mainDiv">
-        <h1>Why are you Studying?</h1>
-      </div>
+        {/* end pick language */}
 
-      <div class="card-deck">
-        <div class="card" onClick={()=>{ selectCardType('work'); }}>
-          <img src = {work_img} class="card-img-top" alt="work"></img>
-          <div class="card-body">
-            <h5 class="card-title">Work</h5>
-            <p class="card-text">Enhance your business trip<br></br>
-            with essential vocabulary.</p>
+        {/* pick reason */}
+        <div class = "mainDiv">
+          <h1>Why are you Studying?</h1>
+        </div>
+
+        <div class="card-deck">
+          <div class="card" onClick={()=>{ selectCardType('work'); }}>
+            <img src = {work_img} class="card-img-top" alt="work"></img>
+            <div class="card-body">
+              <h5 class="card-title">Work</h5>
+              <p class="card-text">Enhance your business trip<br></br>
+              with essential vocabulary.</p>
+            </div>
+          </div>
+          <div class="card" onClick={()=>{ selectCardType('travel'); }}>
+            <img src = {travel_img} class="card-img-top" alt="travel"></img>
+            <div class="card-body">
+              <h5 class="card-title">Travel</h5>
+              <p class="card-text">Travel professionally<br></br>
+              by making the most out of your getaway.</p>
+            </div>
+          </div>
+          <div class="card" onClick={()=>{ selectCardType('culture'); }}>
+            <img src = {culture_img} class="card-img-top" alt="culture"></img>
+            <div class="card-body">
+              <h5 class="card-title">Culture</h5>
+              <p class="card-text">Learn more than just the language.<br></br>
+              Immerse yourself.</p>
+            </div>
           </div>
         </div>
-        <div class="card" onClick={()=>{ selectCardType('travel'); }}>
-          <img src = {travel_img} class="card-img-top" alt="travel"></img>
-          <div class="card-body">
-            <h5 class="card-title">Travel</h5>
-            <p class="card-text">Travel professionally<br></br>
-            by making the most out of your getaway.</p>
-          </div>
+        {/* end pick reason */}
+        <div className = "buttonDiv">
+          <Link to="/Review"><button type="button" class="btn btn-success" onClick={()=>{ updateMaster(); }}>Begin!</button></Link>
         </div>
-        <div class="card" onClick={()=>{ selectCardType('culture'); }}>
-          <img src = {culture_img} class="card-img-top" alt="culture"></img>
-          <div class="card-body">
-            <h5 class="card-title">Culture</h5>
-            <p class="card-text">Learn more than just the language.<br></br>
-            Immerse yourself.</p>
-          </div>
-        </div>
-      </div>
-      {/* end pick reason */}
-      <div className = "buttonDiv">
-        <Link to="/Review"><button type="button" class="btn btn-success" onClick={()=>{ updateMaster(); }}>Begin!</button></Link>
-      </div>
-    </body>
-  );
+      </body>
+    )
+  }
 }
 
 function updateMaster() {
@@ -110,27 +156,7 @@ function updateMaster() {
 }
 
 
-// sets global language variables for submission
-function selectLanguage(langToSelect) { 
-  switch(langToSelect) {
-    case "spanish":
-      spanishClass += " green";
-      !spanish ? spanish = true : spanish = false;
-      break;
-    case "french":
-      !french ? french = true : french = false;
-      break;
-    case "japanese":
-      !japanese ? japanese = true : japanese = false;
-      break;
-    default:
-      break;
-  }
 
-  
-
-  alert("" + spanish + french + japanese);
-}
 
 // sets global study reason variables for submission
 function selectCardType(cardToSelect) { 
