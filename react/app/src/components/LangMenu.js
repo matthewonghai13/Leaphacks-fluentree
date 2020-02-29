c48f2e7a7cde1289fa1a8e5032ca8e2d6f53b751
Matthew Onghai <matthewonghai@utexas.edu>
2020-02-29 14:43:57 -0600

    language selection

--- react/app/src/components/LangMenu.js
+++ react/app/src/components/LangMenu.js
@@ -3,16 +3,87 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import '../stylesheets/LangMenu.css';
 
 
 // login page for webapp
 
+var spanish = false;
+var french = false;
+var japanese = false;
+
+var work = false;
+var travel = false;
+var culture = false;
+
+
 function Splash() {
   return (
     <body>
-      <container>Hello</container>
+      <div class = "mainDiv">
+        <h1>Pick your Language.</h1>
+      </div>
+      <div class="container langSelect">
+        <div class="row">
+          <div class="col-sm spanish" onClick={()=>{ selectLanguage('spanish'); }}>
+            Spanish
+          </div>
+          <div class="col-sm french" onClick={()=>{ selectLanguage('french'); }}>
+            French
+          </div>
+          <div class="col-sm japanese" onClick={()=>{ selectLanguage('japanese'); }}>
+            Japanese
+          </div>
+        </div>
+      </div>
+      <div class = "mainDiv">
+        <h1>Why are you studying?</h1>
+      </div>
+      <div class="container langSelect">
+        <div class="row">
+          <div class="col-sm spanish" onClick={()=>{ selectCardType('work'); }}>
+            Work
+          </div>
+          <div class="col-sm french" onClick={()=>{ selectCardType('travel'); }}>
+            Travel
+          </div>
+          <div class="col-sm japanese" onClick={()=>{ selectCardType('culture'); }}>
+            Culture
+          </div>
+        </div>
+      </div>
     </body>
   );
+}
+
+function selectLanguage(langToSelect) { 
+  switch(langToSelect) {
+    case "spanish":
+      !spanish ? spanish = true : spanish = false;
+      break;
+    case "french":
+      !french ? french = true : french = false;
+      break;
+    case "japanese":
+      !japanese ? japanese = true : japanese = false;
+      break;
+  }
+  alert("" + spanish + french + japanese);
+}
+
+function selectCardType(cardToSelect) { 
+  switch(cardToSelect) {
+    case "work":
+      !work ? work = true : work = false;
+      break;
+    case "travel":
+      !travel ? travel = true : travel = false;
+      break;
+    case "culture":
+      !culture ? culture = true : culture = false;
+      break;
+  }
+  alert("" + work + travel + culture);
 }
 
 export default Splash;
